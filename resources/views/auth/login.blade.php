2979687cb429e40b661c2c84098fecd16623e5da
kaungkhantzin <you@example.com>
2025-03-17 10:49:06 +0630

    login scrow

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,100 +35,79 @@
 </style>
 @endsection
 @section('contents')
-<section class="pt-50 pb-50 bg-blue-2" style="height: 100vh;">
+<section class="pt-50 pb-50 bg-blue-2"> 
     <div class="container">
         <div class="row justify-center">
             <div class="col-xl-6 col-lg-7 col-md-9">
                 <div class="px-50 py-50 sm:px-20 sm:py-20 bg-white shadow-4 rounded-4" style="border-radius: 20px;">
-                    <form id="login_form" action="{{locale_route('login.store') }}" method="POST">
+                    <form id="login_form" action="{{ locale_route('login.store') }}" method="POST">
                         @csrf
                         <div class="row y-gap-20">
                             <div class="col-12">
-                                <h1 class="text-22 fw-500 text-center">{{translate('login')}}</h1>
-                                <p class="text-center">{{translate('welcome')}}</p>
-
+                                <h1 class="text-22 fw-500 text-center">{{ translate('login') }}</h1>
+                                <p class="text-center">{{ translate('welcome') }}</p>
                             </div>
 
+                            <!-- Email Input -->
                             <div class="col-12 input-group">
-
-                                <div class="form-input ">
-                                    <input type="text" name="email" id="email" placeholder="{{translate('email')}}" required>
-
+                                <div class="form-input">
+                                    <input type="text" name="email" id="email" placeholder="{{ translate('email') }}" required>
                                 </div>
-
                                 @error('email')
                                 <span class="invalid-feedback">{{ $message }}</span>
                                 @enderror
                             </div>
 
+                            <!-- Password Input -->
                             <div class="col-12 input-group">
-
-                                <div class="form-input ">
-                                    <input type="password" name="password" id="password" placeholder="{{translate('password')}}" required>
-
+                                <div class="form-input">
+                                    <input type="password" name="password" id="password" placeholder="{{ translate('password') }}" required>
                                 </div>
-
                                 @error('password')
                                 <span class="invalid-feedback">{{ $message }}</span>
                                 @enderror
-
                             </div>
 
-
-
+                            <!-- Submit Button -->
                             <div class="col-12">
-                                <!-- <div class="text-center" id="message">
-                                    @if ($errors->any())
-                                    <span class="invalid-feedback">{{ $errors->first() }}</span>
-                                    @endif
-                                </div> -->
-                            </div>
-
-
-                            <div class="col-12">
-
                                 <button type="submit" class="button py-20 -dark-1 bg-red text-white">
-                                    {{translate('log_in')}}
-                                    <!-- <div class="icon-arrow-top-right ml-15"></div> -->
+                                    {{ translate('log_in') }}
                                 </button>
-
                             </div>
                         </div>
                     </form>
 
+                    <!-- Social Login Section -->
                     <div class="row y-gap-20 pt-30">
                         <div class="col-12">
-                            <div class="text-center">{{translate('or_sign_in_with')}}</div>
+                            <div class="text-center">{{ translate('or_sign_in_with') }}</div>
 
-                            <!-- <button class="button col-12 -line-color text-line-green py-5 rounded-8 mt-10">
-                                <img src="{{ asset('assets/img/icons/line.svg') }}" alt="line" class="mr-10">
-                                {{translate('login_with_line')}}
-                            </button> -->
+                            <!-- LINE Login Button -->
                             <a href="{{ route('auth.line') }}" class="button col-12 -line-color text-line-green py-5 rounded-8 mt-10">
                                 <img src="{{ asset('assets/img/icons/line.svg') }}" alt="line" class="mr-10">
                                 {{ translate('login_with_line') }}
                             </a>
 
+                            <!-- Social Icons -->
                             <div class="login-icons mt-10">
-
                                 <a href="#">
-                                    <img src="{{ asset('assets/img/icons/facebook.svg') }}" alt="">
+                                    <img src="{{ asset('assets/img/icons/facebook.svg') }}" alt="Facebook Login">
                                 </a>
                                 <a href="{{ route('google.login') }}">
                                     <img src="{{ asset('assets/img/icons/google.svg') }}" alt="Google Login">
                                 </a>
                             </div>
-
                         </div>
-                        <p class="mt-10 text-center">{{translate('dont_have_account')}} <a href="{{locale_route('signup') }}"
-                                class="text-blue-1">{{translate('register')}}</a></p>
+
+                        <!-- Registration and Forgot Password Links -->
+                        <p class="mt-10 text-center">{{ translate('dont_have_account') }} <a href="{{ locale_route('signup') }}" class="text-blue-1">{{ translate('register') }}</a></p>
                         <div class="col-12 text-center">
-                            <a href="{{locale_route('forget_password') }}"
-                                class="text-14 fw-500 text-blue-1 underline">{{translate('forgot_password')}}</a>
+                            <a href="{{ locale_route('forget_password') }}" class="text-14 fw-500 text-blue-1 underline">{{ translate('forgot_password') }}</a>
                         </div>
 
+                        <!-- Terms and Conditions -->
                         <div class="col-12">
-                            <div class="text-center px-30">{{translate('agree_terms')}}</div>
+                            <div class="text-center px-30">{{ translate('agree_terms') }}</div>
                         </div>
                     </div>
                 </div>
