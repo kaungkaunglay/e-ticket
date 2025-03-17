208877feba22a6d7568b46a973b34874a5c97b10
kaungkhantzin <you@example.com>
2025-03-17 09:57:18 +0630

    email logo

--- resources/views/emails/vendor_confirmation.blade.php
+++ resources/views/emails/vendor_confirmation.blade.php
@@ -85,7 +85,7 @@
 <body>
     <div class="email-container">
         <div class="header">
-        <img href="https://r-buzz.net/assets/img/general/logo-dark.png" alt="会社ロゴ">
+        <img src="https://res.cloudinary.com/da45qmffk/image/upload/v1742181851/logo-dark_r8vepo_vtcuq7.png" alt="会社ロゴ">
         </div>
         <div class="content">
             <h1>こんにちは、{{ $name }}さん!</h1>
