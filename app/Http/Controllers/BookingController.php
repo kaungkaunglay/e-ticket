d72bb0b36904945f6c5d6a6ef894fb1d15c146a8
swethaharhtun <116005543+swethaharhtun@users.noreply.github.com>
2025-03-22 13:46:16 +0630

    update

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -102,7 +102,7 @@ class BookingController extends Controller
         Mail::to(Auth::user()->email)->send(new BookingConfirmation($booking, Auth::user()));
 
         // Send email to sthahar896@gmail.com
-        Mail::to('sthahar896@gmail.com')->send(new BookingConfirmationAdmin($booking, Auth::user()));
+        Mail::to('kado@and-fun.com')->send(new BookingConfirmationAdmin($booking, Auth::user()));
 
         return redirect()->route('booking.thankyou')->with('success', 'Your booking was successful! A confirmation email has been sent.');
     }
