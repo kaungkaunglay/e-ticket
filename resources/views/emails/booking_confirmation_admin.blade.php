1a5e81c82dd4a8bda33023d60085991a34fcb1d5
swethaharhtun <116005543+swethaharhtun@users.noreply.github.com>
2025-03-22 13:32:15 +0630

    update

--- resources/views/emails/booking_confirmation_admin.blade.php
+++ resources/views/emails/booking_confirmation_admin.blade.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>予約確認 admin</title>
+    <title>管理者メール</title>
     <meta charset="UTF-8">
 </head>
 <body>
-    <h1>ご予約ありがとうございます！</h1>
+    <h1>ユーザーからご予約連絡がありました。</h1>
 
     <!-- <p>{{ $booking->first_name }} 様</p> -->
 
-    <p><strong>{{ $restaurant->name }}</strong> へのご予約が確定いたしました。</p>
+    <h2>ご予約内容：</h2>
 
-    <p><strong>ご予約内容：</strong></p>
+    <p><strong>ユーザー名：</strong> {{ $user->name }}</p>
+
+    <p><strong>メールアドレス：</strong>{{ $user->email }}</p>
+    <p><strong>電話番号 :</strong>{{ $user->phone }}</p>
     <ul>
         <li><strong>店舗名：</strong> {{ $restaurant->name }}</li>
         <li><strong>予約日時：</strong> {{ $booking->select_date }}</li>
         <li><strong>備考：</strong> {{ $booking->note ?? '特別なリクエストなし' }}</li>
     </ul>
 
-    <p>ご来店を心よりお待ちしております。</p>
+    <p>予約のご確認の程宜しくお願い致します。</p>
 
-    <p>どうぞよろしくお願いいたします。</p>
-    <p>{{ $restaurant->name }} スタッフ一同</p>
 </body>
 </html>
